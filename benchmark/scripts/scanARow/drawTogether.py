da3022748f398f1ef2f102a0cadc518f67fcb053
tony <292224750@qq.com>
2023-05-09 17:01:11 +0800

    1. add scripts

---
+++ benchmark/scripts/scanARow/drawTogether.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python3
+import csv
+import numpy as np
+import accuBar as accuBar
+import groupBar as groupBar
+import groupBar2 as groupBar2
+import groupLine as groupLine
+from autoParase import *
+import itertools as it
+import os
+
+import matplotlib
+import numpy as np
+import pylab
+import matplotlib.font_manager as fm
+from matplotlib.font_manager import FontProperties
+from matplotlib.ticker import LogLocator, LinearLocator
+import os
+import pandas as pd
+import sys
+from OoOCommon import *
+import time
+
+# OPT_FONT_NAME = 'Helvetica'
+TICK_FONT_SIZE = 22
+LABEL_FONT_SIZE = 28
+LEGEND_FONT_SIZE = 30
+LABEL_FP = FontProperties(style='normal', size=LABEL_FONT_SIZE)
+LEGEND_FP = FontProperties(style='normal', size=LEGEND_FONT_SIZE)
+TICK_FP = FontProperties(style='normal', size=TICK_FONT_SIZE)
+
+MARKERS = (['*', '|', 'v', "^", "", "h", "<", ">", "+", "d", "<", "|", "", "+", "_"])
+# you may want to change the color map for different figures
+COLOR_MAP = (
+    '#B03A2E', '#2874A6', '#239B56', '#7D3C98', '#FFFFFF', '#F1C40F', '#F5CBA7', '#82E0AA', '#AEB6BF', '#AA4499')
+# you may want to change the patterns for different figures
+PATTERNS = (["////", "o", "", "||", "-", "//", "\\", "o", "O", "////", ".", "|||", "o", "---", "+", "\\\\", "*"])
+LABEL_WEIGHT = 'bold'
+LINE_COLORS = COLOR_MAP
+LINE_WIDTH = 3.0
+MARKER_SIZE = 15.0
+MARKER_FREQUENCY = 1000
+
+matplotlib.rcParams['ps.useafm'] = True
+matplotlib.rcParams['pdf.use14corefonts'] = True
+matplotlib.rcParams['xtick.labelsize'] = TICK_FONT_SIZE
+matplotlib.rcParams['ytick.labelsize'] = TICK_FONT_SIZE
+matplotlib.rcParams['font.family'] = OPT_FONT_NAME
+matplotlib.rcParams['pdf.fonttype'] = 42
+
+scanTag = "aRow"
+
+
+def singleRun(exePath, singleValue, resultPath, configTemplate):
+    # resultFolder="singleValueTests"
+    configFname = "config_" + scanTag + str(singleValue) + ".csv"
+    # configTemplate = "config.csv"
+    # clear old files
+    os.system("cd " + exePath + "&& sudo rm *.csv")
+
+    # editConfig(configTemplate, exePath + configFname, "earlierEmitMs", 0)
+    editConfig(configTemplate, exePath + configFname, scanTag, singleValue)
+    # prepare new file
+    # run
+    os.system("cd " + exePath + "&& sudo ./benchmark " + configFname)
+    # copy result
+    os.system("sudo rm -rf " + resultPath + "/" + str(singleValue))
+    os.system("sudo mkdir " + resultPath + "/" + str(singleValue))
+    os.system("cd " + exePath + "&& sudo cp *.csv " + resultPath + "/" + str(singleValue))
+
+
+def runScanVector(exePath, singleValueVec, resultPath, templateName="config.csv"):
+    for i in singleValueVec:
+        singleRun(exePath, i, resultPath, templateName)
+
+
+def readResultSingle(singleValue, resultPath):
+    resultFname = resultPath + "/" + str(singleValue) + "/default.csv"
+    elapsedTime = readConfig(resultFname, "perfElapsedTime")
+    cacheMiss = readConfig(resultFname, "cacheMiss")
+    cacheRefs = readConfig(resultFname, "cacheRefs")
+    return elapsedTime, cacheMiss, cacheRefs
+
+
+def readResultVector(singleValueVec, resultPath):
+    elapseTimeVec = []
+    cacheMissVec = []
+    cacheRefVec = []
+    for i in singleValueVec:
+        elapsedTime, cacheMiss, cacheRefs = readResultSingle(i, resultPath)
+        elapseTimeVec.append(float(elapsedTime) / 1000.0)
+        cacheMissVec.append(float(cacheMiss))
+        cacheRefVec.append(float(cacheRefs))
+    return elapseTimeVec, cacheMissVec, cacheRefVec
+
+
+def main():
+    exeSpace = os.path.abspath(os.path.join(os.getcwd(), "../..")) + "/"
+    resultPath = os.path.abspath(os.path.join(os.getcwd(), "../..")) + "/results/" + scanTag
+    figPath = os.path.abspath(os.path.join(os.getcwd(), "../..")) + "/figures/" + scanTag
+    configTemplate = exeSpace + "config.csv"
+    valueVec = [100, 200, 500, 1000, 2000, 5000, 10000, 100000]
+    valueVecDisp = np.array(valueVec)
+    print(configTemplate)
+    # run
+    if (len(sys.argv) < 2):
+        os.system("mkdir ../../results")
+        os.system("mkdir ../../figures")
+        os.system("mkdir " + figPath)
+        os.system("sudo rm -rf " + resultPath)
+        os.system("sudo mkdir " + resultPath)
+        runScanVector(exeSpace, valueVec, resultPath, "config.csv")
+    elapseTimeVec, cacheMissVec, cacheRefVec = readResultVector(valueVec, resultPath)
+    # os.system("mkdir " + figPath)
+    groupLine.DrawFigureYnormal([valueVec], [elapseTimeVec],
+                                ['RawFDAMM'],
+                                "#elements in A's row", "elapsed time (ms)", 0, 1, figPath + scanTag + "_elapsedTime",
+                                True)
+
+    # draw2yLine("watermark time (ms)",singleValueVecDisp,lat95Vec,errVec,"95% Latency (ms)","Error","ms","",figPath+"wm_lat")
+    # draw2yLine("watermark time (ms)",singleValueVecDisp,thrVec,errVec,"Throughput (KTp/s)","Error","KTp/s","",figPath+"wm_thr")
+    # draw2yLine("watermark time (ms)",singleValueVecDisp,lat95Vec,compVec,"95% Latency (ms)","Completeness","ms","",figPath+"wm_omp")
+    # groupLine.DrawFigureYnormal([singleValueVec,singleValueVec],[errVec,aqpErrVec],['w/o aqp',"w/ MeanAqp"],"watermark time (ms)","Error",0,1,figPath+"wm_MeanAqp",True)
+    # print(errVec)
+    # print(aqpErrVec)
+    print(elapseTimeVec)
+    # readResultsingleValue(50,resultPath)
+
+
+if __name__ == "__main__":
+    main()
--- benchmark/src/Benchmark.cpp
+++ benchmark/src/Benchmark.cpp
@@ -4,8 +4,6 @@
  * @brief This is the main entry point of the entire program.
  * We use this as the entry point for benchmarking.
  */
-
-#include <torch/torch.h>
 #include <torch/torch.h>
 #include <iostream>
 #include <torch/script.h>
@@ -17,44 +15,44 @@
 #include <Utils/UtilityFunctions.h>
 using namespace std;
 using namespace INTELLI;
-void runSingleThreadTest(std::string configName)
-{
+void runSingleThreadTest(std::string configName) {
   ConfigMapPtr cfg = newConfigMap();
   cfg->fromFile(configName);
-  uint64_t aRow,aCol,bCol,sketchDimension;
-  aRow= cfg->tryU64("aRow", 100, true);
-  aCol= cfg->tryU64("aCol", 1000, true);
-  bCol= cfg->tryU64("bCol", 500, true);
+  uint64_t aRow, aCol, bCol, sketchDimension;
+  aRow = cfg->tryU64("aRow", 100, true);
+  aCol = cfg->tryU64("aCol", 1000, true);
+  bCol = cfg->tryU64("bCol", 500, true);
   sketchDimension = cfg->tryU64("sketchDimension", 50, true);
-  uint64_t coreBind= cfg->tryU64("coreBind", 0, true);
-  UtilityFunctions::bind2Core((int)coreBind);
-  std::string ptFile= cfg->tryString("ptFile", "torchscripts/FDAMM.pt", true);
-  uint64_t customResultName= cfg->tryU64("customResultName", 0, true);
-  INTELLI_INFO("Place me at core"+ to_string(coreBind));
-  INTELLI_INFO("Generating ["+to_string(aRow)+"x"+to_string(aCol)+"]*["+to_string(aCol)+"x"+to_string(bCol)+"], with sketch"+ to_string(sketchDimension));
+  uint64_t coreBind = cfg->tryU64("coreBind", 0, true);
+  UtilityFunctions::bind2Core((int) coreBind);
+  std::string ptFile = cfg->tryString("ptFile", "torchscripts/FDAMM.pt", true);
+  uint64_t customResultName = cfg->tryU64("customResultName", 0, true);
+  INTELLI_INFO("Place me at core" + to_string(coreBind));
+  INTELLI_INFO(
+      "Generating [" + to_string(aRow) + "x" + to_string(aCol) + "]*[" + to_string(aCol) + "x" + to_string(bCol)
+          + "], with sketch" + to_string(sketchDimension));
   torch::jit::script::Module module;
-  INTELLI_INFO("Try pt file "+ ptFile);
+  INTELLI_INFO("Try pt file " + ptFile);
   module = torch::jit::load(ptFile);
   //555
-  auto A = torch::rand({(long)aRow, (long)aCol});
-  auto B = torch::rand({(long)bCol, (long)aCol});
+  auto A = torch::rand({(long) aRow, (long) aCol});
+  auto B = torch::rand({(long) bCol, (long) aCol});
   INTELLI_INFO("Generation done, conducting...");
-  ThreadPerf pef((int)coreBind);
+  ThreadPerf pef((int) coreBind);
   pef.setPerfList();
   pef.start();
-  auto C =module.forward({A, B, (long)sketchDimension}).toTensor();
+  auto C =module.forward({A, B, (long) sketchDimension}).toTensor();
   pef.end();
-  std::string ruName="default";
-  if(customResultName)
-  {
-   ruName= "ru_core_"+to_string(coreBind)+"_matrix_"+to_string(aRow)+"x"+to_string(aCol)+"amm"+to_string(aCol)+"x"+to_string(bCol)+"_sketch_"+to_string(sketchDimension);
+  std::string ruName = "default";
+  if (customResultName) {
+    ruName = "ru_core_" + to_string(coreBind) + "_matrix_" + to_string(aRow) + "x" + to_string(aCol) + "amm"
+        + to_string(aCol) + "x" + to_string(bCol) + "_sketch_" + to_string(sketchDimension);
   }
 
-
-  auto resultCsv=pef.resultToConfigMap();
-  resultCsv->toFile(ruName+".csv");
+  auto resultCsv = pef.resultToConfigMap();
+  resultCsv->toFile(ruName + ".csv");
   INTELLI_INFO("Done. here is result");
-  std::cout<<resultCsv->toString()<<endl;
+  std::cout << resultCsv->toString() << endl;
 }
 
 int main(int argc, char **argv) {
