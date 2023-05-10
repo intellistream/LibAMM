013b71119b529baff280c70f0669537e98211e4c
tony <292224750@qq.com>
2023-05-10 19:27:09 +0800

    1. add the sparse matrix loader and corresponding scripts

--- src/MatrixLoader/AbstractMatrixLoader.cpp
+++ src/MatrixLoader/AbstractMatrixLoader.cpp
@@ -5,16 +5,13 @@
 #include <MatrixLoader/AbstractMatrixLoader.h>
 
 //do nothing in abstract class
-bool AMMBench::AbstractMatrixLoader::setConfig(INTELLI::ConfigMapPtr cfg)
-{
+bool AMMBench::AbstractMatrixLoader::setConfig(INTELLI::ConfigMapPtr cfg) {
   assert(cfg);
   return true;
 }
-torch::Tensor AMMBench::AbstractMatrixLoader::getA()
-{
-  return  torch::rand({1,1});
+torch::Tensor AMMBench::AbstractMatrixLoader::getA() {
+  return torch::rand({1, 1});
 }
-torch::Tensor AMMBench::AbstractMatrixLoader::getB()
-{
-  return  torch::rand({1,1});
+torch::Tensor AMMBench::AbstractMatrixLoader::getB() {
+  return torch::rand({1, 1});
 }
