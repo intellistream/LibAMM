5dc6f44c87ee5d20a4716228c0067b416abf5fd8
tony <292224750@qq.com>
2023-05-10 14:23:58 +0800

    1. add a MatrixLoader component for loading matrixes 2. introduce the doxygen system

--- benchmark/src/Benchmark.cpp
+++ benchmark/src/Benchmark.cpp
@@ -1,17 +1,10 @@
-// Copyright (C) 2021 by the IntelliStream team (https://github.com/intellistream)
+/*! \file Benchmark.h*/
 
 /**
  * @brief This is the main entry point of the entire program.
  * We use this as the entry point for benchmarking.
  */
-#include <torch/torch.h>
-#include <iostream>
-#include <torch/script.h>
-#include <string>
-#include <memory>
-#include <Utils/ConfigMap.hpp>
-#include <Utils/IntelliLog.h>
-#include <Utils/ThreadPerf.hpp>
+#include <AMMBench.h>
 #include <Utils/UtilityFunctions.h>
 using namespace std;
 using namespace INTELLI;
@@ -19,27 +12,29 @@ using namespace torch;
 void runSingleThreadTest(std::string configName) {
   ConfigMapPtr cfg = newConfigMap();
   cfg->fromFile(configName);
-  //555
-  uint64_t aRow, aCol, bCol, sketchDimension;
-  aRow = cfg->tryU64("aRow", 100, true);
-  aCol = cfg->tryU64("aCol", 1000, true);
-  bCol = cfg->tryU64("bCol", 500, true);
+  AMMBench::MatrixLoaderTable mLoaderTable;
+  uint64_t  sketchDimension;
   sketchDimension = cfg->tryU64("sketchDimension", 50, true);
   uint64_t coreBind = cfg->tryU64("coreBind", 0, true);
   UtilityFunctions::bind2Core((int) coreBind);
   std::string ptFile = cfg->tryString("ptFile", "torchscripts/FDAMM.pt", true);
-  uint64_t customResultName = cfg->tryU64("customResultName", 0, true);
+  //uint64_t customResultName = cfg->tryU64("customResultName", 0, true);
   INTELLI_INFO("Place me at core" + to_string(coreBind));
   INTELLI_INFO(
-      "Generating [" + to_string(aRow) + "x" + to_string(aCol) + "]*[" + to_string(aCol) + "x" + to_string(bCol)
-          + "], with sketch" + to_string(sketchDimension));
+      "with sketch" + to_string(sketchDimension));
   torch::jit::script::Module module;
   INTELLI_INFO("Try pt file " + ptFile);
   module = torch::jit::load(ptFile);
-  torch::manual_seed(114514);
+  std::string matrixLoaderTag=cfg->tryString("matrixLoaderTag", "random", true);
+  auto matLoaderPtr=mLoaderTable.findMatrixLoader(matrixLoaderTag);
+  assert(matLoaderPtr);
+  matLoaderPtr->setConfig(cfg);
+  auto A = matLoaderPtr->getA();
+  auto B = matLoaderPtr->getB();
+      /*torch::manual_seed(114514);
   //555
   auto A = torch::rand({(long) aRow, (long) aCol});
-  auto B = torch::rand({(long) aCol, (long) bCol});
+  auto B = torch::rand({(long) aCol, (long) bCol});*/
   INTELLI_INFO("Generation done, conducting...");
   ThreadPerf pef((int) coreBind);
   pef.setPerfList();
@@ -47,10 +42,7 @@ void runSingleThreadTest(std::string configName) {
   auto C =module.forward({A, B, (long) sketchDimension}).toTensor();
   pef.end();
   std::string ruName = "default";
-  if (customResultName) {
-    ruName = "ru_core_" + to_string(coreBind) + "_matrix_" + to_string(aRow) + "x" + to_string(aCol) + "amm"
-        + to_string(aCol) + "x" + to_string(bCol) + "_sketch_" + to_string(sketchDimension);
-  }
+
 
   auto resultCsv = pef.resultToConfigMap();
   resultCsv->toFile(ruName + ".csv");
---
+++ include/MatrixLoader/AbstractMatrixLoader.h
@@ -0,0 +1,77 @@
+/*! \file AbstractMatrixLoader.h*/
+//
+// Created by tony on 10/05/23.
+//
+
+#ifndef INTELLISTREAM_INCLUDE_MATRIXLOADER_ABSTRACTMATRIXLOADER_H_
+#define INTELLISTREAM_INCLUDE_MATRIXLOADER_ABSTRACTMATRIXLOADER_H_
+
+#include <Utils/ConfigMap.hpp>
+#include <assert.h>
+#include <torch/torch.h>
+#include <memory>
+namespace AMMBench {
+/**
+ * @ingroup AMMBENCH_MatrixLOADER
+ * {
+ */
+/**
+ * @ingroup AMMBENCH_MatrixLOADER_abstract The abstract template
+ * {
+ */
+/**
+ * @class AbstractMatrixLoader MatrixLoader/AbstractMatrixLoader.h
+ * @brief The abstract class of matrix loader, parent for all loaders
+ * @ingroup AMMBENCH_MatrixLOADER_abstract
+ * @note:
+ * - Must have a global config by @ref setConfig
+ *@note  Default behavior
+* - create
+* - call @ref setConfig, this function will also generate the tensor A and B correspondingly
+* - call @ref getA and @ref getB (assuming we are benchmarking torch.mm(A,B))
+ */
+class AbstractMatrixLoader {
+ public:
+  AbstractMatrixLoader() = default;
+
+  ~AbstractMatrixLoader() = default;
+  /**
+     * @brief Set the GLOBAL config map related to this loader
+     * @param cfg The config map
+      * @return bool whether the config is successfully set
+      * @note
+     */
+  virtual bool setConfig(INTELLI::ConfigMapPtr cfg);
+  /**
+   * @brief get the A matrix
+   * @return the generated A matrix
+   */
+  virtual torch::Tensor getA();
+  /**
+  * @brief get the B matrix
+  * @return the generated B matrix
+  */
+  virtual torch::Tensor getB();
+};
+/**
+ * @ingroup AMMBENCH_MatrixLOADER_abstract
+ * @typedef AbstractMatrixLoaderPtr
+ * @brief The class to describe a shared pointer to @ref AbstractMatrixLoader
+
+ */
+typedef std::shared_ptr<class AMMBench::AbstractMatrixLoader> AbstractMatrixLoaderPtr;
+/**
+ * @ingroup AMMBENCH_MatrixLOADER_abstract
+ * @def newAbstractMatrixLoader
+ * @brief (Macro) To creat a new @ref AbstractMatrixLoader under shared pointer.
+ */
+#define newAbstractMatrixLoader std::make_shared<AMMBench::AbstractMatrixLoader>
+/**
+ * @}
+ */
+/**
+ * @}
+ */
+} // AMMBench
+
+#endif //INTELLISTREAM_INCLUDE_MATRIXLOADER_ABSTRACTMATRIXLOADER_H_
