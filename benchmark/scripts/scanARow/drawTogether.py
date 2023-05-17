52bdd497910905cd0337d29c3abcdd015503f10b
tony <292224750@qq.com>
2023-05-17 17:20:11 +0800

    1. add examples of system test 2. fix multi threading bugs on some machines (e.g.., the aarach64), force a single tread now by  torch::set_num_threads(1);

--- benchmark/scripts/scanARow/drawTogether.py
+++ benchmark/scripts/scanARow/drawTogether.py
@@ -81,9 +81,12 @@ def readResultSingle(singleValue, resultPath):
     cacheRefs = readConfig(resultFname, "cacheRefs")
     return elapsedTime, cacheMiss, cacheRefs
 
+
 def cleanPath(path):
     os.system("sudo rm -rf " + path)
     os.system("sudo mkdir " + path)
+
+
 def readResultVector(singleValueVec, resultPath):
     elapseTimeVec = []
     cacheMissVec = []
@@ -99,13 +102,13 @@ def readResultVector(singleValueVec, resultPath):
 def main():
     exeSpace = os.path.abspath(os.path.join(os.getcwd(), "../..")) + "/"
     resultPath = os.path.abspath(os.path.join(os.getcwd(), "../..")) + "/results/" + scanTag
-    resultPathFDAMM = os.path.abspath(os.path.join(os.getcwd(), "../..")) + "/results/" + scanTag+"/FDAMM"
-    resultPathCoFD = os.path.abspath(os.path.join(os.getcwd(), "../..")) + "/results/" + scanTag+"/CoFD"
-    resultPathBetaCoFD = os.path.abspath(os.path.join(os.getcwd(), "../..")) + "/results/" + scanTag+"/BCoFD"
-    resultPathRAWMM = os.path.abspath(os.path.join(os.getcwd(), "../..")) + "/results/" + scanTag+"/RAWMM"
+    resultPathFDAMM = os.path.abspath(os.path.join(os.getcwd(), "../..")) + "/results/" + scanTag + "/FDAMM"
+    resultPathCoFD = os.path.abspath(os.path.join(os.getcwd(), "../..")) + "/results/" + scanTag + "/CoFD"
+    resultPathBetaCoFD = os.path.abspath(os.path.join(os.getcwd(), "../..")) + "/results/" + scanTag + "/BCoFD"
+    resultPathRAWMM = os.path.abspath(os.path.join(os.getcwd(), "../..")) + "/results/" + scanTag + "/RAWMM"
     figPath = os.path.abspath(os.path.join(os.getcwd(), "../..")) + "/figures/" + scanTag
     configTemplate = exeSpace + "config.csv"
-    valueVec = [100, 200, 500, 1000, 2000, 5000, 10000, 20000,50000]
+    valueVec = [100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000]
     valueVecDisp = np.array(valueVec)
     print(configTemplate)
     # run
@@ -125,18 +128,20 @@ def main():
         runScanVector(exeSpace, valueVec, resultPathCoFD, "config_CoAMM.csv")
         runScanVector(exeSpace, valueVec, resultPathBetaCoFD, "config_BCoAMM.csv")
         runScanVector(exeSpace, valueVec, resultPathRAWMM, "config_RAWMM.csv")
-    evaTypes=['FDAMM','MM','Co-FD','BCO-FD']
+    evaTypes = ['FDAMM', 'MM', 'Co-FD', 'BCO-FD']
     elapseTimeVecFD, cacheMissVecFD, cacheRefVecFD = readResultVector(valueVec, resultPathFDAMM)
     elapseTimeVecCoFD, cacheMissVecCoFD, cacheRefVecCoFD = readResultVector(valueVec, resultPathCoFD)
     elapseTimeVeCB, cacheMissVecB, cacheRefVecB = readResultVector(valueVec, resultPathBetaCoFD)
     elapseTimeVecRAW, cacheMissVecRAW, cacheRefVecRAW = readResultVector(valueVec, resultPathRAWMM)
     # os.system("mkdir " + figPath)
-    groupLine.DrawFigure([valueVec,valueVec,valueVec,valueVec], [elapseTimeVecFD,elapseTimeVecRAW,elapseTimeVecCoFD,elapseTimeVeCB],
-                                evaTypes,
-                                "#elements in A's row", "elapsed time (ms)", 0, 1, figPath + scanTag + "_elapsedTime",
-                                True)
-    groupLine.DrawFigureYnormal([valueVec,valueVec,valueVec,valueVec], [cacheMissVecFD/cacheRefVecFD*100.0,cacheMissVecRAW/cacheRefVecRAW*100.0,
-                                                                        cacheMissVecCoFD/cacheRefVecCoFD*100.0,cacheMissVecB/cacheRefVecB*100.0],
+    groupLine.DrawFigure([valueVec, valueVec, valueVec, valueVec],
+                         [elapseTimeVecFD, elapseTimeVecRAW, elapseTimeVecCoFD, elapseTimeVeCB],
+                         evaTypes,
+                         "#elements in A's row", "elapsed time (ms)", 0, 1, figPath + scanTag + "_elapsedTime",
+                         True)
+    groupLine.DrawFigureYnormal([valueVec, valueVec, valueVec, valueVec],
+                                [cacheMissVecFD / cacheRefVecFD * 100.0, cacheMissVecRAW / cacheRefVecRAW * 100.0,
+                                 cacheMissVecCoFD / cacheRefVecCoFD * 100.0, cacheMissVecB / cacheRefVecB * 100.0],
                                 evaTypes,
                                 "#elements in A's row", "cacheMiss (%)", 0, 1, figPath + scanTag + "_cacheMiss",
                                 True)
@@ -146,7 +151,7 @@ def main():
     # groupLine.DrawFigureYnormal([singleValueVec,singleValueVec],[errVec,aqpErrVec],['w/o aqp',"w/ MeanAqp"],"watermark time (ms)","Error",0,1,figPath+"wm_MeanAqp",True)
     # print(errVec)
     # print(aqpErrVec)
-    #print(elapseTimeVecFD)
+    # print(elapseTimeVecFD)
     # readResultsingleValue(50,resultPath)
 
 
