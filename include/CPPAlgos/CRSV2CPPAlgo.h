c057739689395ed2f00d279b01d700a6a6beeff8
haolan <haolan_he@sutd.edu.sg>
2023-05-28 07:49:16 +0000

    add Column Row Sampling C++ implementation version 2 (CRS v2)

---
+++ include/CPPAlgos/CRSV2CPPAlgo.h
@@ -0,0 +1,58 @@
+//
+// Created by haolan on 5/26/23.
+//
+
+#ifndef INTELLISTREAM_CRSV2CPPALGO_H
+#define INTELLISTREAM_CRSV2CPPALGO_H
+
+#include <CPPAlgos/AbstractCPPAlgo.h>
+
+namespace AMMBench {
+/**
+ * @ingroup AMMBENCH_CppAlgos The algorithms writtrn in c++
+ * @{
+ */
+/**
+ * @class CRSCPPlgo CPPAlgos/CRSV2CPPAlgo.h
+ * @brief The cloumn row sampling (CRS) class of c++ algos
+ *
+ */
+    class CRSV2CPPAlgo : public AMMBench::AbstractCPPAlgo {
+    public:
+        CRSV2CPPAlgo() {
+
+        }
+
+        ~CRSV2CPPAlgo() {
+
+        }
+
+        /**
+         * @brief the virtual function provided for outside callers, rewrite in children classes
+         * @param A the A matrix
+         * @param B the B matrix
+         * @param sketchSize the size of sketc or sampling
+         * @return the output c matrix
+         */
+        virtual torch::Tensor amm(torch::Tensor A, torch::Tensor B, int sketchSize);
+
+    };
+
+/**
+ * @ingroup AMMBENCH_CppAlgos
+ * @typedef AbstractMatrixCppAlgoPtr
+ * @brief The class to describe a shared pointer to @ref CRSV2CppAlgo
+
+ */
+    typedef std::shared_ptr<class AMMBench::CRSV2CPPAlgo> CRSV2CPPAlgoPtr;
+/**
+ * @ingroup AMMBENCH_CppAlgos
+ * @def newCRSV2CppAlgo
+ * @brief (Macro) To creat a new @ref  CRSV2CppAlgounder shared pointer.
+ */
+#define newCRSV2CPPAlgo std::make_shared<AMMBench::CRSV2CPPAlgo>
+}
+/**
+ * @}
+ */
+#endif //INTELLISTREAM_CRSV2CPPALGO_H
--- src/CPPAlgos/CPPAlgoTable.cpp
+++ src/CPPAlgos/CPPAlgoTable.cpp
@@ -4,10 +4,12 @@
 
 #include <CPPAlgos/CPPAlgoTable.h>
 #include <CPPAlgos/CRSCPPAlgo.h>
+#include <CPPAlgos/CRSV2CPPAlgo.h>
 namespace AMMBench {
 AMMBench::CPPAlgoTable::CPPAlgoTable() {
   algoMap["mm"] = newAbstractCPPAlgo();
   algoMap["crs"] = newCRSCPPAlgo();
+    algoMap["crsV2"] = newCRSV2CPPAlgo();
 }
 
 } // AMMBench
