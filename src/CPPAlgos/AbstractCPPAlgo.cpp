885ca4e9b5c8f8b64a0996c82b5f7fefa5e4aaf2
tony <292224750@qq.com>
2023-06-05 21:55:59 +0800

    1. fix the uint64_t bugs 2. Note; please note the int8 is just a toy

--- src/CPPAlgos/AbstractCPPAlgo.cpp
+++ src/CPPAlgos/AbstractCPPAlgo.cpp
@@ -6,5 +6,6 @@
 #include <CPPAlgos/AbstractCPPAlgo.h>
 torch::Tensor AMMBench::AbstractCPPAlgo::amm(torch::Tensor A, torch::Tensor B, uint64_t sketchSize) {
   std::cout << sketchSize;
+  INTELLI_INFO("I am mm");
   return torch::matmul(A, B);
 }
--- src/CPPAlgos/EWSCPPAlgo.cpp
+++ src/CPPAlgos/EWSCPPAlgo.cpp
@@ -5,11 +5,11 @@
 #include <CPPAlgos/EWSCPPAlgo.h>
 
 namespace AMMBench {
-torch::Tensor AMMBench::EWSCPPAlgo::amm(torch::Tensor A, torch::Tensor B, int k) {
+torch::Tensor AMMBench::EWSCPPAlgo::amm(torch::Tensor A, torch::Tensor B, uint64_t k2) {
     auto A_size = A.sizes();
     int64_t m = A_size[0];
     int64_t n = A_size[1];
-
+    int64_t k=(int64_t) k2;
     TORCH_CHECK(n == B.size(0));
     TORCH_CHECK(k < n);
 
