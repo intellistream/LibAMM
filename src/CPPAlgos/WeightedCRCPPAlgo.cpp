d8c94fa71dbaef9e00ecc295384d1a37ea51fca1
heyyuhao <yuhaoheee@gmail.com>
2023-06-08 14:00:17 +0800

    change weighted-cr probability distribution to matrix form, and fix some small issues

--- src/CPPAlgos/WeightedCRCPPAlgo.cpp
+++ src/CPPAlgos/WeightedCRCPPAlgo.cpp
@@ -9,13 +9,14 @@ namespace AMMBench {
 torch::Tensor AMMBench::WeightedCRCPPAlgo::amm(torch::Tensor A, torch::Tensor B, uint64_t c) {
   
   int64_t n = A.size(1); // A: m*n, B: n*d
+  std::cout << "A shape: " << A.sizes() << std::endl;
+  std::cout << "B shape: " << B.sizes() << std::endl;
 
   // Probability distribution
-  torch::Tensor probability_distribution = torch::zeros({n});
-    for (int i = 0; i < n; ++i) {
-      probability_distribution[i] = torch::norm(A.t()[i]) * torch::norm(B[i]);
-    }
-    probability_distribution /= probability_distribution.sum();
+  torch::Tensor col_norm_A = torch::norm(A, /*p=*/2, /*dim=*/0); // norm on columns of A
+  torch::Tensor row_norm_B = torch::norm(B, /*p=*/2, /*dim=*/1); // norm on rows of B
+  torch::Tensor probability_distribution = torch::mul(col_norm_A, row_norm_B);
+  probability_distribution /= probability_distribution.sum();
 
   // S
   torch::Tensor sample_indices = torch::multinomial(probability_distribution, /*num_samples*/c, /*replacement*/true);
@@ -24,11 +25,8 @@ torch::Tensor AMMBench::WeightedCRCPPAlgo::amm(torch::Tensor A, torch::Tensor B,
   std::tie(unique_indices, _, occurences) = at::_unique2(sample_indices, /*sorted*/false, /*return_inverse*/false, /*return_counts*/true);
 
   torch::Tensor S = torch::zeros({n, unique_indices.size(0)});
-
-  for (int trial = 0; trial < unique_indices.size(0); ++trial) {
-    int index = unique_indices[trial].item<int>();
-    S[index][trial] = 1;
-  }
+  torch::Tensor trial = torch::arange(unique_indices.size(0));
+  S.index_put_({unique_indices, trial}, 1);
 
   // D
   torch::Tensor D = torch::diag(torch::sqrt(occurences) / torch::sqrt(torch::tensor({static_cast<long>(c)}, torch::kLong) * probability_distribution.index_select(0, unique_indices)));
