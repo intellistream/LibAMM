1. fix latency calculation bugs

benchmark/pythonTest.py
@@ -9,11 +9,11 @@ def main():
     b = torch.rand(100, 100)
     # The pytorch +
     print('/****test add****/')
-    print('pytorch+:', torch.matmul(a,b))
+    print('pytorch-mm:', torch.matmul(a,b))
     # our c++ extension of +
     torch.ops.load_library("../libIntelliStream.so")
     torch.ops.AMMBench.setTag('mm')
-    print('AMMBench-CRS+:', torch.ops.AMMBench.ammDefault(a, b))
+    print('AMMBench-MM+:', torch.ops.AMMBench.ammDefault(a, b))
     torch.ops.AMMBench.setTag('crs')
     print('AMMBench-CRS+:', torch.ops.AMMBench.ammDefault(a, b))
 