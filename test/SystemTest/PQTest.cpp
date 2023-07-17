e8694a832beaa39e6b407684159e401a5b213fef
darthnoward <me@darthnoward.com>
2023-07-17 20:23:22 +0800

    add Vector Quantization

--- test/SystemTest/PQTest.cpp
+++ test/SystemTest/PQTest.cpp
@@ -38,3 +38,18 @@ TEST_CASE("Test PQ Hash", "[short]")
     double froError = INTELLI::UtilityFunctions::relativeFrobeniusNorm(realC, ammC);
     REQUIRE(froError < 0.5);
 }
+
+TEST_CASE("Test VQ", "[short]")
+{
+    torch::manual_seed(114514);
+    AMMBench::VectorQuantization vq;
+    auto A = torch::rand({1000, 1000});
+    auto B = torch::rand({1000, 1000});
+    auto realC = torch::matmul(A, B);
+    auto ammC = vq.amm(A, B, 1000);
+    torch::Tensor first_row = ammC[0];
+    std::cout << first_row << std::endl;
+    double froError = INTELLI::UtilityFunctions::relativeFrobeniusNorm(realC, ammC);
+    std::cout << froError << std::endl;
+    REQUIRE(froError < 0.5);
+}
