0bf4ba6a4c273e8f20a5d3b0b243a452c32f6c5d
heyuhao <yuhaoheee@gmail.com>
2023-08-10 07:42:09 +0000

    for paper5.1, set rank=0.1, no repeat svd calculation

--- src/CPPAlgos/BlockLRACPPAlgo.cpp
+++ src/CPPAlgos/BlockLRACPPAlgo.cpp
@@ -9,8 +9,8 @@
 
 namespace AMMBench {
 void AMMBench::BlockLRACPPAlgo::setConfig(INTELLI::ConfigMapPtr cfg) {
-  ARankRatio = cfg->tryDouble("algoARankRatio", 0.5, true);
-  BRankRatio = cfg->tryDouble("algoBRankRatio", 0.5, true);
+  ARankRatio = cfg->tryDouble("algoARankRatio", 0.1, true);
+  BRankRatio = cfg->tryDouble("algoBRankRatio", 0.1, true);
 }
 
 torch::Tensor AMMBench::BlockLRACPPAlgo::amm(torch::Tensor A, torch::Tensor B, uint64_t blockSize) {
@@ -32,59 +32,57 @@ torch::Tensor AMMBench::BlockLRACPPAlgo::amm(torch::Tensor A, torch::Tensor B, u
   // for each block, calculate LRA
   torch::Tensor finalLRA = torch::zeros({(long) m, (long) n});
 
+  std::vector<std::vector<std::tuple<torch::Tensor, torch::Tensor, torch::Tensor>>> svdA_blocks(m / blockSize, std::vector<std::tuple<torch::Tensor, torch::Tensor, torch::Tensor>>(k / blockSize));
+
+  std::vector<std::vector<std::tuple<torch::Tensor, torch::Tensor, torch::Tensor>>> svdB_blocks(k / blockSize, std::vector<std::tuple<torch::Tensor, torch::Tensor, torch::Tensor>>(n / blockSize));
+
   for (uint64_t I = 0; I < m / blockSize; ++I) {
-    for (uint64_t J = 0; J < n / blockSize; ++J) {
-      torch::Tensor subFinalLRA = torch::zeros({(long) blockSize, (long) blockSize});
       for (uint64_t K = 0; K < k / blockSize; ++K) {
-        // get sub matrix
-        torch::Tensor AIK = A.index({torch::indexing::Slice(I * blockSize, (I + 1) * blockSize),
-                                     torch::indexing::Slice(K * blockSize, (K + 1) * blockSize)});
-        torch::Tensor BKJ = B.index({torch::indexing::Slice(K * blockSize, (K + 1) * blockSize),
-                                     torch::indexing::Slice(J * blockSize, (J + 1) * blockSize)});
-        // SVD
-        torch::Tensor UA;
-        torch::Tensor SA;
-        torch::Tensor VhA;
-        torch::Tensor UB;
-        torch::Tensor SB;
-        torch::Tensor VhB;
-        std::tie(UA, SA, VhA) = torch::linalg::svd(AIK, false, c10::nullopt);
-        std::tie(UB, SB, VhB) = torch::linalg::svd(BKJ, false, c10::nullopt);
-        // LRA
-        torch::Tensor UATruncated = UA.narrow(1, 0, ARankRatio * blockSize);
-        torch::Tensor SATruncated = torch::diag(SA.narrow(0, 0, ARankRatio * blockSize));
-        torch::Tensor VhATruncated = VhA.narrow(0, 0, ARankRatio * blockSize);
-        torch::Tensor UBTruncated = UB.narrow(1, 0, BRankRatio * blockSize);
-        torch::Tensor SBTruncated = torch::diag(SB.narrow(0, 0, BRankRatio * blockSize));
-        torch::Tensor VhBTruncated = VhB.narrow(0, 0, BRankRatio * blockSize);
-        // MM
-        subFinalLRA += torch::matmul(torch::matmul(UATruncated,
-                                                   torch::matmul(torch::matmul(SATruncated,
-                                                                               torch::matmul(VhATruncated,
-                                                                                             UBTruncated)),
-                                                                 SBTruncated)), VhBTruncated);
+          torch::Tensor AIK = A.index({torch::indexing::Slice(I * blockSize, (I + 1) * blockSize),
+                                        torch::indexing::Slice(K * blockSize, (K + 1) * blockSize)});
+          torch::Tensor UA, SA, VhA;
+          std::tie(UA, SA, VhA) = torch::linalg::svd(AIK, false, c10::nullopt);
+          torch::Tensor UATruncated = UA.narrow(1, 0, ARankRatio * blockSize);
+          torch::Tensor SATruncated = torch::diag(SA.narrow(0, 0, ARankRatio * blockSize));
+          torch::Tensor VhATruncated = VhA.narrow(0, 0, ARankRatio * blockSize);
+          svdA_blocks[I][K] = std::make_tuple(UATruncated, SATruncated, VhATruncated);
       }
-      finalLRA.index_put_({torch::indexing::Slice(I * blockSize, (I + 1) * blockSize),
-                           torch::indexing::Slice(J * blockSize, (J + 1) * blockSize)}, subFinalLRA);
-    }
+  }
+
+  for (uint64_t K = 0; K < k / blockSize; ++K) {
+      for (uint64_t J = 0; J < n / blockSize; ++J) {
+          torch::Tensor BKJ = B.index({torch::indexing::Slice(K * blockSize, (K + 1) * blockSize),
+                                        torch::indexing::Slice(J * blockSize, (J + 1) * blockSize)});
+          torch::Tensor UB, SB, VhB;
+          std::tie(UB, SB, VhB) = torch::linalg::svd(BKJ, false, c10::nullopt);
+          torch::Tensor UBTruncated = UB.narrow(1, 0, BRankRatio * blockSize);
+          torch::Tensor SBTruncated = torch::diag(SB.narrow(0, 0, BRankRatio * blockSize));
+          torch::Tensor VhBTruncated = VhB.narrow(0, 0, BRankRatio * blockSize);
+          svdB_blocks[K][J] = std::make_tuple(UBTruncated, SBTruncated, VhBTruncated);
+      }
+  }
+
+  for (uint64_t I = 0; I < m / blockSize; ++I) {
+      for (uint64_t J = 0; J < n / blockSize; ++J) {
+          torch::Tensor subFinalLRA = torch::zeros({(long) blockSize, (long) blockSize});
+          for (uint64_t K = 0; K < k / blockSize; ++K) {
+              const auto& svdA = svdA_blocks[I][K];
+              const auto& svdB = svdB_blocks[K][J];
+              torch::Tensor UATruncated = std::get<0>(svdA);
+              torch::Tensor SATruncated = std::get<1>(svdA);
+              torch::Tensor VhATruncated = std::get<2>(svdA);
+              torch::Tensor UBTruncated = std::get<0>(svdB);
+              torch::Tensor SBTruncated = std::get<1>(svdB);
+              torch::Tensor VhBTruncated = std::get<2>(svdB);
+              subFinalLRA += torch::matmul(torch::matmul(UATruncated, 
+                              torch::matmul(torch::matmul(SATruncated,
+                                torch::matmul(VhATruncated,UBTruncated)), SBTruncated)), VhBTruncated);
+          }
+
+          finalLRA.index_put_({torch::indexing::Slice(I * blockSize, (I + 1) * blockSize),
+                              torch::indexing::Slice(J * blockSize, (J + 1) * blockSize)}, subFinalLRA);
+      }
   }
 
   return finalLRA;
-}
-} // AMMBench
-
-
-// int main() {
-
-//   torch::manual_seed(114514);
-//   AMMBench::BlockLRACPPAlgo wcr;
-//   auto A = torch::rand({500, 400});
-//   auto B = torch::rand({400, 600});
-//   auto realC = torch::matmul(A, B);
-//   auto ammC = wcr.amm(A, B, 100, 0.5, 0.5);
-//   double froError = INTELLI::UtilityFunctions::relativeFrobeniusNorm(realC, ammC);
-//   std::cout << "froError: " << froError << std::endl;
-
-//   return 0;
-// }
-
+}}
