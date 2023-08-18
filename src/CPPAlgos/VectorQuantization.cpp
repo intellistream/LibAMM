c60dea31667a807d4916c31f482ccd5f13975d3e
heyuhao <yuhaoheee@gmail.com>
2023-08-18 07:35:16 +0000

    fix pq in streaming

--- src/CPPAlgos/VectorQuantization.cpp
+++ src/CPPAlgos/VectorQuantization.cpp
@@ -29,6 +29,7 @@ void AMMBench::VectorQuantization::setConfig(INTELLI::ConfigMapPtr cfg) {
     }
 
 torch::Tensor AMMBench::VectorQuantization::amm(const torch::Tensor A, const torch::Tensor B, uint64_t l) {
+    
     l=0;
     int A_rows = A.sizes()[0];
     int B_cols = B.sizes()[1];
@@ -59,7 +60,7 @@ torch::Tensor AMMBench::VectorQuantization::amm(const torch::Tensor A, const tor
 
     // Convert vectors to tensors for matrices A and B
     torch::Tensor A_quantized_tensor = torch::stack(A_quantized, 1);
-    std::cout << "A_quantized_tensor shape: " << A_quantized_tensor.sizes() << std::endl;
+    // std::cout << "A_quantized_tensor shape: " << A_quantized_tensor.sizes() << std::endl;
 
     // Find the nearest codeword indices for matrix B
     for (int k = 0; k < m; ++k) {
@@ -74,7 +75,7 @@ torch::Tensor AMMBench::VectorQuantization::amm(const torch::Tensor A, const tor
 
     // Convert vectors to tensors for matrices A and B
     torch::Tensor B_quantized_tensor = torch::stack(B_quantized, 0);
-    std::cout << "B_quantized_tensor shape: " << B_quantized_tensor.sizes() << std::endl;
+    // std::cout << "B_quantized_tensor shape: " << B_quantized_tensor.sizes() << std::endl;
 
     // Define the batch size for batch processing
     int batch_size_A = A_rows;
@@ -87,18 +88,14 @@ torch::Tensor AMMBench::VectorQuantization::amm(const torch::Tensor A, const tor
     for (int i = 0; i < A_rows; i += batch_size_A) {
         for (int j = 0; j < B_cols; j += batch_size_B) {
             torch::Tensor batch_result = torch::zeros({batch_size_A, batch_size_B});
-            
             for (int k = 0; k < m; ++k) {
                 // Gather quantized indices for the current batch
-                torch::Tensor A_indices = A_quantized_tensor.slice(1, i, i + batch_size_A).select(1, k);
-                torch::Tensor B_indices = B_quantized_tensor[k].slice(0, j, j + batch_size_B);
-                
+                torch::Tensor A_indices = A_quantized_tensor.slice(0, i, i + batch_size_A).select(1, k);
+                torch::Tensor B_indices = B_quantized_tensor.slice(1, j, j + batch_size_B)[k];
                 // Broadcast batch indices to match the shape of B_indices
                 auto batch_indices = A_indices.unsqueeze(1).expand({-1, B_indices.sizes()[0]});
-
                 // Gather elements from A using batch and B_indices
                 torch::Tensor batch_lookup = lookUpTable[k].index({batch_indices, B_indices});
-                
                 // Accumulate the batch result
                 batch_result += batch_lookup;
             }
