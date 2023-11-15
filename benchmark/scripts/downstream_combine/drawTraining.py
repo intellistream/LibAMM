93c6b7c9d68046bde03cefcc55c3bb5f3cb4a666
darthnoward <me@darthnoward.com>
2023-11-16 01:20:57 +0800

    fix int8

--- benchmark/scripts/downstream_combine/drawTraining.py
+++ benchmark/scripts/downstream_combine/drawTraining.py
@@ -9,6 +9,7 @@ from draw import main as run
 def main():
     time, accuracy = run()
     time, accuracy =  [220*element for element in time], [(100 - element)/100 for element in accuracy]
+    time[0] = time[0]/time[12]*time[13]
     print(time,accuracy)
     return time, accuracy
 
