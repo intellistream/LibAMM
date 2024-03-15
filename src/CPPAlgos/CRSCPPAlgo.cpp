c3a88c58ad37ea04cc0cf58c79fb80fdaa569fd4
tony <292224750@qq.com>
2024-03-15 14:49:59 +0800

    1. add the cuda version to smp-pca

--- src/CPPAlgos/CRSCPPAlgo.cpp
+++ src/CPPAlgos/CRSCPPAlgo.cpp
@@ -9,31 +9,33 @@ namespace AMMBench {
 torch::Tensor AMMBench::CRSCPPAlgo::amm(torch::Tensor A, torch::Tensor B, uint64_t k) {
   torch::Tensor C;
   auto start = std::chrono::high_resolution_clock::now();
-  A = A.t();
-  torch::Tensor B_sampled;
-  //INTELLI_INFO("I am CPP-CRS");
-  int64_t n = A.size(0);
-  //int64_t m = A.size(1);
 
-  assert(n == B.size(0));
-  // Probability distribution
-  torch::Tensor probs = torch::ones(n) / n;  // default: uniform
 
-  // Sample k indices from range 0 to n for given probability distribution
-  torch::Tensor indices = torch::multinomial(probs, k, true);
-
-  // Sample k columns from A
-  torch::Tensor A_sampled = A.index_select(0, indices);
-  // int64_t ratio = std::ceil(static_cast<double>(n) / k);
-  // A_sampled = (A_sampled / (int) k).t().div(probs.index_select(0, torch::arange(0, n, ratio)));
-  A_sampled = (A_sampled / (int) k).t().div(torch::ones(1) / n);
 
   // Sample k rows from B
 
   if (useCuda) {
     INTELLI_INFO("CRS under cuda!");
-    auto ac = A_sampled.to(torch::kCUDA);
+    // Probability distribution
+    int64_t n = A.size(0);
+    torch::Tensor probs = torch::ones(n) / n;  // default: uniform
+    probs=probs.to(torch::kCUDA);
+    A = A.to(torch::kCUDA);
+    B = B.to(torch::kCUDA);
     buildATime = chronoElapsedTime(start);
+    torch::Tensor B_sampled;
+    A = A.t();
+    // Sample k indices from range 0 to n for given probability distribution
+    torch::Tensor indices = torch::multinomial(probs, k, true);
+    indices=indices.to(torch::kCUDA);
+    // Sample k columns from A
+    torch::Tensor A_sampled = A.index_select(0, indices);
+    // int64_t ratio = std::ceil(static_cast<double>(n) / k);
+    // A_sampled = (A_sampled / (int) k).t().div(probs.index_select(0, torch::arange(0, n, ratio)));
+    A_sampled = (A_sampled / (int) k).t().div(torch::ones(1,torch::kCUDA) / n);
+
+    auto ac = A_sampled.to(torch::kCUDA);
+
     B_sampled = B.index_select(0, indices);
     auto bc = B_sampled.to(torch::kCUDA);
     buildBTime = chronoElapsedTime(start) - buildATime;
@@ -42,6 +44,24 @@ torch::Tensor AMMBench::CRSCPPAlgo::amm(torch::Tensor A, torch::Tensor B, uint64
     C = cc.to(torch::kCPU);
     postProcessTime = chronoElapsedTime(start) - buildATime - buildBTime - fABTime;
   } else {
+    torch::Tensor B_sampled;
+    A = A.t();
+    //INTELLI_INFO("I am CPP-CRS");
+    int64_t n = A.size(0);
+    //int64_t m = A.size(1);
+
+    assert(n == B.size(0));
+    // Probability distribution
+    torch::Tensor probs = torch::ones(n) / n;  // default: uniform
+
+    // Sample k indices from range 0 to n for given probability distribution
+    torch::Tensor indices = torch::multinomial(probs, k, true);
+
+    // Sample k columns from A
+    torch::Tensor A_sampled = A.index_select(0, indices);
+    // int64_t ratio = std::ceil(static_cast<double>(n) / k);
+    // A_sampled = (A_sampled / (int) k).t().div(probs.index_select(0, torch::arange(0, n, ratio)));
+    A_sampled = (A_sampled / (int) k).t().div(torch::ones(1) / n);
     buildATime = chronoElapsedTime(start);
     B_sampled = B.index_select(0, indices);
     buildBTime = chronoElapsedTime(start) - buildATime;
